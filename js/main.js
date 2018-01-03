c1f5e337e83ab2b414df9f875e7393b6e790a1fa
ykxie <ykxie@kaikeba.com>
2018-01-03 19:35:24 +0800

    add some

--- js/main.js
+++ js/main.js
@@ -9,5 +9,24 @@ $(function(){
             flag = 1
         }
     })
+    var index = $('.menu li').index($('m-active'))
+    console.log('index', index)
+    
+    // $(".menu li a").mouseenter(function(){
+    //     console.log($('.menu li').index())
 
+    //     $(this).addClass('m-active')
+    // });
+    // $(".menu li a").mouseleave(function(){
+    //     console.log('menu')
+    //     $(this).removeClass('m-active')
+    // });
+    $(".divisions").mouseenter(function(){
+        console.log('0000')
+        $('.d-menu').show();
+    });
+    $(".divisions").mouseleave(function(){
+        console.log('1111')
+        $('.d-menu').hide();
+    });
 })
